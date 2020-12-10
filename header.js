f649735102e7c58ec538fd7046fc7fbb1bd4f4df
Michele Catasta <pirroh@cs.stanford.edu>
2020-12-10 00:10:08 -0800

    CS246 skeleton website.

---
+++ header.js
@@ -0,0 +1,42 @@
+document.write('\
+\
+<!-- Navbar -->\
+<nav class="navbar navbar-default navbar-fixed-top">\
+  <div class="container">\
+      <div class="navbar-header">\
+        <a class="navbar-brand brand" href="index.html">CS246</a>\
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+          </button>\
+      </div>\
+\
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\
+\
+          <ul class="nav navbar-nav navbar-right">\
+              <li><a href="index.html#content">Content</a></li>\
+              <li><a href="index.html#schedule">Schedule</a></li>\
+              <li><a href="info.html">Course Info</a></li>\
+              <li><a href="oh.html">Office Hours</a></li>\
+              <li><a href="faq.html">FAQ</a></li>\
+              <li><a href="https://www.gradescope.com/courses/78454">Gradescope</a></li>\
+              <li><a href="https://campuswire.com/c/GF955CA72/">Campuswire</a></li>\
+          </ul>\
+      </div>\
+  </div>\
+</nav>\
+\
+<!-- Header -->\
+<div id="header" style="text-align:center">\
+  <a href="http://snap.stanford.edu/">\
+    <img src="images/snaplogo.png" class="logo-left">\
+  </a>\
+  <a href="http://stanford.edu/">\
+    <img src="images/stanfordlogo.png" class="logo-right">\
+  </a>\
+  <h1>CS246: Mining Massive Data Sets</h1>\
+  <h3>Stanford / Winter 2020</h3>\
+  <div style="clear:both;"></div>\
+</div>\
+');
