3089d5345b9230e13eb46fca874f14ba3b2fa5b6
Chip <chip@huyenchip.com>
2020-12-13 10:42:04 -0800

    First push

--- header.js
+++ header.js
@@ -4,7 +4,7 @@ document.write('\
 <nav class="navbar navbar-default navbar-fixed-top">\
   <div class="container">\
       <div class="navbar-header">\
-        <a class="navbar-brand brand" href="index.html">CS246</a>\
+        <a class="navbar-brand brand" href="index.html">CS 329S</a>\
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">\
                 <span class="icon-bar"></span>\
                 <span class="icon-bar"></span>\
@@ -15,13 +15,9 @@ document.write('\
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\
 \
           <ul class="nav navbar-nav navbar-right">\
-              <li><a href="index.html#content">Content</a></li>\
-              <li><a href="index.html#schedule">Schedule</a></li>\
-              <li><a href="info.html">Course Info</a></li>\
-              <li><a href="oh.html">Office Hours</a></li>\
-              <li><a href="faq.html">FAQ</a></li>\
-              <li><a href="https://www.gradescope.com/courses/78454">Gradescope</a></li>\
-              <li><a href="https://campuswire.com/c/GF955CA72/">Campuswire</a></li>\
+              <li><a href="index.html#overview">Overview</a></li>\
+              <li><a href="syllabus.html">Syllabus</a></li>\
+              <li><a href="index.html#faq">FAQ</a></li>\
           </ul>\
       </div>\
   </div>\
@@ -29,14 +25,11 @@ document.write('\
 \
 <!-- Header -->\
 <div id="header" style="text-align:center">\
-  <a href="http://snap.stanford.edu/">\
-    <img src="images/snaplogo.png" class="logo-left">\
-  </a>\
   <a href="http://stanford.edu/">\
     <img src="images/stanfordlogo.png" class="logo-right">\
   </a>\
-  <h1>CS246: Mining Massive Data Sets</h1>\
-  <h3>Stanford / Winter 2020</h3>\
+  <h1>CS 329S: Machine Learning Systems Design</h1>\
+  <h3>Stanford, Winter 2021</h3>\
   <div style="clear:both;"></div>\
 </div>\
 ');
