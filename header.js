4a75ff0a870b52c05a22ce87c367ea5c967c3f36
Chip <chip@huyenchip.com>
2022-03-11 17:16:54 -0500

    Initial commit

---
+++ header.js
@@ -0,0 +1,37 @@
+document.write('\
+\
+<!-- Navbar -->\
+<nav class="navbar navbar-default navbar-fixed-top">\
+  <div class="container">\
+      <div class="navbar-header">\
+        <a class="navbar-brand brand" href="https://cs329s.stanford.edu/index.html">CS 329S</a>\
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+          </button>\
+      </div>\
+\
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\
+\
+          <ul class="nav navbar-nav navbar-right">\
+              <li><a href="index.html#overview">Overview</a></li>\
+              <li><a href="syllabus.html">Syllabus</a></li>\
+              <li><a href="index.html#faq">FAQ</a></li>\
+              <li><a href="https://stanford-cs329s.github.io/2021/">Past course</a></li>\
+              <li><a href="https://stanford-cs329s.github.io/reports/">Past projects</a></li>\
+          </ul>\
+      </div>\
+  </div>\
+</nav>\
+\
+<!-- Header -->\
+<div id="header" style="text-align:center">\
+  <a href="http://stanford.edu/">\
+    <img src="images/stanfordlogo.png" class="logo-right">\
+  </a>\
+  <h1>CS 329S: Machine Learning Systems Design</h1>\
+  <h3>Stanford, Winter 2022</h3>\
+  <div style="clear:both;"></div>\
+</div>\
+');
